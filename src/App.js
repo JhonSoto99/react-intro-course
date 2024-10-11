9e0dc41fb396945cb766a83c51141c055147c868
Jhon Soto <jhon.soto@aflore.co>
2024-10-11 17:34:51 -0500

    feat(components): Create new components.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from "./TodoCounter";
+import {TodoSearch} from "./TodoSearch";
+import {TodoItem} from "./TodoItem";
+import {CreateTodoButton} from "./CreateTodoButton";
+import {TodoList} from "./TodoList";
 
 function App() {
   return (
     <div className="App">
+        <TodoCounter />
+        <TodoSearch />
 
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+        <TodoList>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </TodoList>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
+        <CreateTodoButton />
+
     </div>
-  );
-}
-
-function TodoItem() {
-  return (
-      <li>
-        <span>V</span>
-        <p>Llorar con la Llorona</p>
-        <span>X</span>
-      </li>
   );
 }
 
