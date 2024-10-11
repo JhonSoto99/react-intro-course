248848c97a8697895fbdc34b9b3a5df341084f90
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-10-11 18:52:58 -0500

    feat(components): Do the dinamycs components.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import {TodoSearch} from "./TodoSearch";
 import {TodoItem} from "./TodoItem";
 import {CreateTodoButton} from "./CreateTodoButton";
 import {TodoList} from "./TodoList";
+import React from "react";
+
+const defaultTodos = [
+    {text: 'Cortar cebolla', completed: false},
+    {text: 'Curso React', completed: true},
+    {text: 'Llorar', completed: false},
+    {text: 'Lalalal', completed: true},
+]
 
 function App() {
   return (
-    <div className="App">
-        <TodoCounter />
+    <React.Fragment>
+        <TodoCounter completed={16} total={10} />
         <TodoSearch />
 
         <TodoList>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {defaultTodos.map(todo =>(
+                <TodoItem
+                    key={todo.text}
+                    completed={todo.completed}
+                    text={todo.text} />
+            ))}
         </TodoList>
 
         <CreateTodoButton />
 
-    </div>
+    </React.Fragment>
   );
 }
 
