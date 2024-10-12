b93f1edb7e572f5591181a980eb618a243253341
Jhon Soto <jhon.soto@aflore.co>
2024-10-12 09:53:38 -0500

    chore(folder-structure): reorganize folder structure.

--- src/TodoIcon/CompleteIcon.js
+++ src/TodoIcon/CompleteIcon.js
@@ -1,4 +1,4 @@
-import {TodoIcon} from "./TodoIcon";
+import {TodoIcon} from "./index";
 
 function CompleteIcon({completed, onClick}) {
     return (
--- src/TodoIcon/DeleteIcon.js
+++ src/TodoIcon/DeleteIcon.js
@@ -1,4 +1,4 @@
-import {TodoIcon} from "./TodoIcon";
+import {TodoIcon} from "./index";
 
 function DeleteIcon({onClick}) {
     return  <TodoIcon type="delete" color="gray" onClick={onClick}/>;
