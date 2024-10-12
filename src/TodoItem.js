37e216af0c997f34d57d62e55b9efb9b90d34239
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-10-12 08:02:54 -0500

    feat(dynamic-componenets): Add dynamic to components.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,17 @@
 import "./TodoItem.css"
 
-function TodoItem({completed, text}) {
+function TodoItem({completed, text, onComplete, onDelete}) {
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>V</span>
+            <span
+                className={`Icon Icon-check ${completed && "Icon-check--active"}`}
+                onClick={onComplete}
+            >V</span>
             <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >X</span>
         </li>
     );
 }
